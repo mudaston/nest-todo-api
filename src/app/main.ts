731729b716bf00d216e45249fd8c4f2c55491f3b
mudaston <twil.revaas@gmail.com>
2023-07-07 00:11:24 +0300

    take app port from .env

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from '@app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = Number(process.env.PORT);
+
+  await app.listen(PORT || 3000);
 }
 bootstrap();
