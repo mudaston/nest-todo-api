60aca59f45e14a4e6854532c956c106a30ec714e
mudaston <twil.revaas@gmail.com>
2023-07-08 16:57:38 +0300

    move main files to src directory

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,13 @@
+import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  const PORT = Number(process.env.PORT);
+  await app.listen(PORT || 3000);
+}
+bootstrap();
