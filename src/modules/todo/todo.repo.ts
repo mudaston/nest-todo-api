792913bb1cba567b8ba87f12c12d63e8d86615c3
mudaston <twil.revaas@gmail.com>
2023-07-08 20:51:26 +0300

    create repository for todo POST service

---
+++ src/modules/todo/todo.repo.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+
+import { PrismaService } from '@app/database';
+
+import type { CreateTodo } from './types';
+
+@Injectable()
+export class TodoRepo {
+  constructor(private readonly orm: PrismaService) {}
+
+  async createTodo(params: CreateTodo) {
+    const { data } = params;
+
+    const todo = await this.orm.todo.create({ data });
+
+    return todo;
+  }
+}
