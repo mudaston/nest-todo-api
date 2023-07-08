6aca49e738731b648965dac9f543d29b61a1574e
mudaston <twil.revaas@gmail.com>
2023-07-08 15:18:37 +0300

    create prisma client module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from '@app/app.controller';
 import { AppService } from '@app/app.service';
+import { PrismaModule } from '@app/database';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
